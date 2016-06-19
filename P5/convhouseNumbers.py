bcbe0249e5a8a51cb74177632d20f6dc5346843a
Ubuntu <ubuntu@ip-172-26-3-33.ec2.internal>
2016-06-19 04:13:49 +0000

    submission v1

--- P5/convhouseNumbers.py
+++ P5/convhouseNumbers.py
@@ -10,7 +10,7 @@
 # 
 # ##### For visualization of the house numbers images you can have a look at the site provided above
 
-# In[27]:
+# In[1]:
 
 import tensorflow as tf
 import scipy.io as scp
@@ -18,23 +18,25 @@ import numpy as np
 import random
 
 
-# In[28]:
+# In[2]:
 
 # Load the data 
 testData = scp.loadmat('../../data/svhn/test_32x32.mat')
 trainData = scp.loadmat('../../data/svhn/train_32x32.mat')
 
+logs_path = '/home/ubuntu/tensorFlowLogs1/'
 
-# In[29]:
 
-testDataX = testData['X']
+# In[3]:
+
+testDataX = testData['X'].astype('float32') / 128.0 - 1         
 testDataY = testData['y']
 
-trainDataX = trainData['X']
+trainDataX = trainData['X'].astype('float32') / 128.0 - 1
 trainDataY = trainData['y']
 
 
-# In[30]:
+# In[4]:
 
 print type(trainDataX)
 print type(trainDataY)
@@ -45,7 +47,7 @@ print 'test data image shape : ', testDataX.shape
 print 'test data output shape : ', testDataY.shape
 
 
-# In[31]:
+# In[5]:
 
 # try tansposing the array
 def transposeArray(data):
@@ -58,7 +60,7 @@ def transposeArray(data):
     return xtrain
 
 
-# In[32]:
+# In[6]:
 
 trainDataX = transposeArray(trainDataX)
 testDataX = transposeArray(testDataX)
@@ -67,7 +69,7 @@ testDataX = transposeArray(testDataX)
 print 'New train data image shape : ', trainDataX.shape
 
 
-# In[33]:
+# In[7]:
 
 def OnehotEndoding(Y):
     Ytr=[]
@@ -83,22 +85,24 @@ def OnehotEndoding(Y):
             temp[3]=1
         elif el==4:
             temp[4]=1
-        elif temp[5]==1:
+        elif el==5:
             temp[5]=1
-        elif temp[6]==1:
+        elif el==6:
             temp[6]=1
-        elif temp[7]==1:
+        elif el==7:
             temp[7]=1
-        elif temp[8]==1:
+        elif el==8:
             temp[8]=1
-        elif temp[9]==1:
+        elif el==9:
             temp[9]=1
         Ytr.append(temp)
         
     return np.asarray(Ytr)
 
 
-# In[34]:
+# ##### Converting the label to one hot encoding as the prediction really improves. This make sense as well. 
+
+# In[8]:
 
 # convert y to one hot encoding
 trainDataY = OnehotEndoding(trainDataY)
@@ -107,7 +111,7 @@ print 'train data output shape : ', trainDataY.shape
 print 'test data output shape : ', testDataY.shape
 
 
-# In[35]:
+# In[9]:
 
 #Neural network parameters
 height = 32
@@ -116,28 +120,29 @@ channel = 3
 tags = 10
 patch = 5
 depth = 16
-num_hidden = 64
+num_hidden = 128
 dropout = 0.75 # Dropout, probability to keep units
 
-learning_rate = 0.00001
+learning_rate = 1e-4
 
 
-# In[36]:
+# In[10]:
 
-tf_X = tf.placeholder("float", shape=[None, height, width, channel])
-tf_Y = tf.placeholder("float", shape=[None, tags])
+stddev = 1e-1
+tf_X = tf.placeholder("float", shape=[None, height, width, channel], name = "X-Input")
+tf_Y = tf.placeholder("float", shape=[None, tags], name = "LabeledData")
 
-convW1 = tf.Variable(tf.random_normal([patch, patch, channel, depth], stddev=0.1))
-bias1 = tf.Variable(tf.random_normal([depth], stddev=0.1))
+convW1 = tf.Variable(tf.random_normal([patch, patch, channel, depth], stddev=stddev), name="ConvW1")
+bias1 = tf.Variable(tf.random_normal([depth], stddev=stddev), name="Bias1")
 
-convW2 = tf.Variable(tf.random_normal([patch, patch, depth, depth], stddev=0.1))
-bias2 = tf.Variable(tf.random_normal([depth], stddev=0.1))
+convW2 = tf.Variable(tf.random_normal([patch, patch, depth, depth], stddev=stddev), name="ConvW2")
+bias2 = tf.Variable(tf.random_normal([depth], stddev=stddev), name = "Bias2")
 
-w3 = tf.Variable(tf.random_normal([height // 4 * width // 4 * depth, num_hidden], stddev=0.1))
-bias3 = tf.Variable(tf.random_normal([num_hidden]))
+w3 = tf.Variable(tf.random_normal([height // 4 * width // 4 * depth, num_hidden], stddev=stddev), name="w3")
+bias3 = tf.Variable(tf.random_normal([num_hidden]), name="bias3")
 
-w4 = tf.Variable(tf.random_normal([num_hidden, tags], stddev=0.1))
-bias4 = tf.Variable(tf.random_normal([tags], stddev=0.1))  
+w4 = tf.Variable(tf.random_normal([num_hidden, tags], stddev=stddev), name="w4")
+bias4 = tf.Variable(tf.random_normal([tags], stddev=stddev), name="bias4")  
 
 keep_prob = tf.placeholder(tf.float32) #dropout (keep probability)
 
@@ -157,7 +162,7 @@ keep_prob = tf.placeholder(tf.float32) #dropout (keep probability)
 # 
 # Fourth Hidden Layer : 64 x 10
 
-# In[37]:
+# In[11]:
 
 #model
 
@@ -189,38 +194,72 @@ def model(X):
     return tf.matmul(dp5, w4) + bias4
 
 
-# In[38]:
+# In[12]:
 
-pred = model(tf_X)
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, tf_Y))
+with tf.name_scope('Model'):
+    pred = model(tf_X)
+with tf.name_scope('loss'):
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, tf_Y))
+
 
 # Optimizer.
-optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
-accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(pred,1),tf.argmax(tf_Y,1)), "float"))
+with tf.name_scope('AdamOptimizer'):
+    optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+    
+with tf.name_scope('accuracy'):
+    accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(tf.nn.softmax(pred),1),tf.argmax(tf_Y,1)), "float"))
+    
+# Create a summary to monitor cost tensor
+tf.scalar_summary("loss", loss)
+# Create a summary to monitor accuracy tensor
+tf.scalar_summary("accuracy", accuracy)
+# Merge all summaries into a single op
+merged_summary_op = tf.merge_all_summaries()
 
 
-# In[ ]:
+# In[13]:
 
 def Accuracy(X, Y, message, sess):    
     print message, sess.run(accuracy, feed_dict= {tf_X: X, tf_Y: Y, keep_prob:1.0})
 
 
-# In[ ]:
+# In[14]:
 
 with tf.Session() as sess:
     tf.initialize_all_variables().run()
-    epoch = 10000
-    batch_size = 500
+    
+    # op to write logs to Tensorboard
+    summary_writer = tf.train.SummaryWriter(logs_path, graph=tf.get_default_graph())
+    
+    epoch = 50000
+    batch_size = 128
     print('Initialized')
     
     p = np.random.permutation(range(len(trainDataX)))
     trX, trY = trainDataX[p], trainDataY[p]
+    start = 0
+    end = 0
     
     for step in range(epoch):
+        start = end
+        end = start + batch_size
         
-        batch = np.random.choice(len(trainDataX) - 1, batch_size)
-        inX, outY = trainDataX[batch], trainDataY[batch]
-        sess.run(optimizer, feed_dict= {tf_X: inX, tf_Y: outY, keep_prob:0.75})
+        if start >= len(trainDataX):
+            start = 0
+            end = start + batch_size
+            
+        if end >= len(trainDataX):
+            end = len(trainDataX) - 1
+        if start == end:
+            start = 0
+            end = start + batch_size
+        
+        #print step, start, end
+            
+        #batch = np.random.choice(len(trainDataX) - 1, batch_size)
+        inX, outY = trX[start:end], trY[start:end]
+        _, summary = sess.run([optimizer, merged_summary_op], feed_dict= {tf_X: inX, tf_Y: outY, keep_prob:0.75})
+        summary_writer.add_summary(summary, step)
         
         if step % 500 == 0:
             print 'cost at each step :', step, 'is :', sess.run(loss, feed_dict={tf_X: inX, tf_Y: outY, keep_prob:1.0})
